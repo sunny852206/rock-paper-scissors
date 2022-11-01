041d4465709f56ec8dd852bf28f94625b587005a
sunny852206 <sunny.pslau@outlook.com>
2022-10-31 21:00:46 -0700

    added playRound function to play a single round RPC

--- script.js
+++ script.js
@@ -1,8 +1,49 @@
 let rockPaperScissors = ["rock", "paper", "scissors"];
 
+// generate random selection from 'rock', 'paper' or 'scissors' for computer
 function getComputerChoice() {
   let random = Math.floor(Math.random() * rockPaperScissors.length);
   let computerChoice = rockPaperScissors[random];
-  console.log(computerChoice);
+  console.log("Comp pick: ", computerChoice);
   return computerChoice;
 }
+
+// play a single round of rock paper scissors
+function playRound(playerSelection, computerSelection) {
+  //   let playerSelection = playerSelection.toLowerCase();
+  if (playerSelection == "rock") {
+    if (computerSelection == "rock") {
+      return "Draw, you both picked Rock!";
+    } else if (computerSelection == "paper") {
+      console.log("You Lose... Paper beats Rock");
+      return "Lose";
+    } else {
+      console.log("win");
+      return "You Win! Rock beats Scissors!";
+    }
+  }
+
+  if (playerSelection == "paper") {
+    if (computerSelection == "rock") {
+      return "You Win! Paper beats Rock!";
+    } else if (computerSelection == "paper") {
+      return "Draw, you both picked Paper!";
+    } else {
+      return "You Lose... Scissors beats Paper";
+    }
+  }
+
+  if (playerSelection == "scissors") {
+    if (computerSelection == "rock") {
+      return "You Lose... Rock beats Scissors";
+    } else if (computerSelection == "paper") {
+      return "You Win! Scissors beat Paper";
+    } else {
+      return "Draw, you both picked Scissors!";
+    }
+  }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
